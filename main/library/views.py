cfd9437d5d5455ea0c0248963b0eed556cd3b53b
doman <manhthang1279@gmail.com>
2021-07-29 17:57:58 +0700

    master

--- main/library/views.py
+++ main/library/views.py
@@ -16,3 +16,18 @@ def index(request):
         'context': context,
         'table_filter': table_filter
     })
+
+# def index(request):
+#     if request.method == 'POST':
+#         table_filter = request.POST.getlist('table_filter')
+#     else:
+#         table_filter = BookFilter(request.GET, queryset=Book.objects.all())
+#
+#     paginator = Paginator(table_filter.qs, 50)
+#     page_number = request.GET.get('page')
+#     context = paginator.get_page(page_number)
+#
+#     return render(request, 'library/index.html', {
+#         'context': context,
+#         'table_filter': table_filter
+#     })
